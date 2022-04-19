4049e23464b00425571ac87cf22cf7831ce8e0dd
Kaiser, Anton <anton.kaiser@student.fhws.de>
2022-04-19 11:32:40 +0200

    Initial commit: added Spring Boot files

---
+++ src/main/java/de/fhws/simplex/SimplexApplication.java
@@ -0,0 +1,13 @@
+package de.fhws.simplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplexApplication.class, args);
+	}
+
+}
