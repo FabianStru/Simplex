822552a5350eb15b17331f68f373fa3f81c7ca97
Braun, Hannes <hannes.braun@student.fhws.de>
2022-04-21 15:03:16 +0200

    Kommentar für Navbar und Code aufgeräumt

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Dropdown from "./Dropdown";
 
+{/* Die Navigationsbar, welche das Dropdown menü enthältt */}
+
+
 function Navbar(){
     return(
         <ul>
-            <Dropdown></Dropdown>
+            <Dropdown/>
             <li> Test</li>
             <li> ich hab 0 Dunst was ich mach</li>
         </ul>
