3f79d2f366396e358bdb45b94ebc20a25e53a823
Braun, Hannes <hannes.braun@student.fhws.de>
2022-04-21 15:03:00 +0200

    Kommentar für Mainpage

--- frontend/src/Mainpage/Mainpage.js
+++ frontend/src/Mainpage/Mainpage.js
@@ -1,10 +1,10 @@
 import React from "react";
 
+{/* Der Homescreen der erscheint beim Aufrufen der webseite */}
 
 const Mainpage = ({}) => {
     return (
         <h1>dreckiger affe</h1>
-
     )
 }
 
