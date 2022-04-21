134fbed4951a0880a796609618aab102e34ba5e4
Braun, Hannes <hannes.braun@student.fhws.de>
2022-04-21 15:02:46 +0200

    added Play screen

---
+++ frontend/src/Mainpage/Play.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+{/* Hier soll der user das Simplex Problem lösen können */}
+
+const Play = ({}) => {
+
+    return(
+        <h1>hässlicher affe</h1>
+    )
+}
+export default Play
