ee9c0de53960ff2fa19c68fa4b93cc20065e2ef2
Kaiser, Anton <anton.kaiser@student.fhws.de>
2022-05-01 20:49:36 +0200

    Added getter and setters for Spring Boot to automatically create JSONs and returned Matrix

--- src/main/java/de/fhws/simplex/SimplexController.java
+++ src/main/java/de/fhws/simplex/SimplexController.java
@@ -1,20 +1,23 @@
 package de.fhws.simplex;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class SimplexController {
 
 
-    @RequestMapping("/home")
-    public String home(Matrix matrix) {
-        matrix.beRottisBeispiel();
+    @GetMapping("/home")
+    public Matrix home(Matrix matrix) {
+//        matrix.beRottisBeispiel();
         matrix.printMatrix();
+        System.out.println(matrix);
         System.out.println("Hi");
-        return "Springboot_Test.html";
+        return matrix;
     }
 }
