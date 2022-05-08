ecf0d2db4d8bd89240d7771e6b69abc90e9f73e9
Kunkel, Lorenz <lorenz.kunkel@student.fhws.de>
2022-05-08 19:52:49 +0200

    Verlinkung zu Ranked erstellt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './App.css';
 import Navbar from "./Navbar/Navbar";
-import Mainpage from "./Mainpage/Mainpage"
-import Play from "./Mainpage/Play"
-import ReactDOM from "react-dom/client";
+import Mainpage from "./Mainpage/Mainpage";
+import Play from "./Mainpage/Play";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import NavItem from './Navbar/NavItem';
+import Ranked from './Mainpage/Ranked';
+
 
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
                         {/* Hier werden die verschiedenen Wege zu den Mainscreens definiert */}
                         <Route path="/" element={(<Mainpage/>)}/>
                         <Route path="/play" element={(<Play/>)}/>
+                        <Route path='/ranked' element={(<Ranked/>)}/>
 
                         {/* das wird angezeigt wenn die Falsche URl eingegeben wird. No touchy, als ich das erstellt hab gings auf einmal sonst bugts  */}
                         <Route path="/*" element={(<h1>This is the wrong page</h1>)}/>
@@ -26,7 +27,7 @@ function App() {
             </div>
         </div>
     )
-        ;
+
 }
 
-export default App;
+export default App
