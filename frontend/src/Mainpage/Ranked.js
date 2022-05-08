db6d2cd735f1551dcd32c07e2eae741102a9cc4b
Braun, Hannes <hannes.braun@student.fhws.de>
2022-05-08 20:04:34 +0200

    build new Tabelle in Ranked.js

--- frontend/src/Mainpage/Ranked.js
+++ frontend/src/Mainpage/Ranked.js
@@ -1,15 +1,16 @@
 import jsonToTabelle from "./Tabelle/JsonToTabelle";
 import React from "@types/react";
+import Button from "../Button/Button";
 
 function Ranked(){
     return(
         <div>
-            <h1>
-                {jsonToTabelle()[0]}
-                {jsonToTabelle()[1]}
-                {jsonToTabelle()[2]}
-                {jsonToTabelle()[3]}
-            </h1>
+            <div>
+                <Tabelle editable="false" Spalteninput="0" Zeileninput="0"/>
+            </div>
+
+
         </div>
+
     )
 }
