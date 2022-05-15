e813813b1048f4759abee4ee888ddf4b5a8b8716
Kunkel, Lorenz <lorenz.kunkel@student.fhws.de>
2022-05-15 17:19:10 +0200

    handleChange der Tabellenfelder gefixt

--- frontend/src/Mainpage/Tabelle/Tabelle.js
+++ frontend/src/Mainpage/Tabelle/Tabelle.js
@@ -6,43 +6,29 @@ const Tabelle = ({Zeileninput, Spalteninput, editable, TableData}) => {
 
     const ZeilenAnzahl = Zeileninput
     const SpaltenAnzahl = Spalteninput
-    /*
-    const [matrix, setMatrix] = useState(Array.from({length: ZeilenAnzahl},()=> Array.from({length: SpaltenAnzahl}, () => null)));
-
-    const handleChange = (row, column, event) => {
-        let copy = [...matrix];
-        copy[row][column] = +event.target.value;
-        setMatrix(copy);
-
-        console.log(matrix);
-    };
-ab hier meins
     let userTable= new Array(ZeilenAnzahl);
     for(let a=0;a<ZeilenAnzahl;a++){
-        userTable[a]=new Array(4)
+        userTable[a]=new Array(3)
     }
 
-    const [userTable , setUserTable]= useState([ZeilenAnzahl][SpaltenAnzahl])
-userTable[Zeilennummer][e.key]=e.target.value
-     */
+    const handleChange = (row, column, event) => {
+        let copy = [...userTable];
+        copy[row][column] = +event.target.value;
+        userTable=copy
+
+        console.log(userTable);
+    };
 
 
     function generateZeile(Zeilennummer) {
-        /*
-        function onChange(e){
-
-            handleChange(Zeilennummer, e.target.key, e)
-
-        }
-
-         */
 
         const Zeile = []
         for (let i = 0; i < SpaltenAnzahl; i++) {
+            const key = i+Zeilennummer
             if (editable) {
-                Zeile.push(<td key={i}><Tabellenfeld editable={editable} title="test" /*onChange={onChange}*//></td>)
+                Zeile.push(<td key={key}><Tabellenfeld editable={editable} onChange={(e)=>handleChange(Zeilennummer,i,e)}/></td>)
             } else {
-                Zeile.push(<td key={i}><Tabellenfeld content={TableData[Zeilennummer][i]} editable={editable}
+                Zeile.push(<td key={key}><Tabellenfeld content={TableData[Zeilennummer][i]} editable={editable}
                                                      title="test"/></td>)
             }
         }
