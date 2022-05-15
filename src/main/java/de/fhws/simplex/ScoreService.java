75e0572b1465fbd7f2efe295cd177b446351345f
Kaiser, Anton <anton.kaiser@student.fhws.de>
2022-05-15 19:52:04 +0200

    Added ScoreService with CRUD methods

---
+++ src/main/java/de/fhws/simplex/ScoreService.java
@@ -0,0 +1,66 @@
+package de.fhws.simplex;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class ScoreService {
+
+    @Autowired
+    private ScoreRepository scoreRepository;
+
+    @Transactional
+    public String createScore(Score score) {
+        try {
+            scoreRepository.save(score);
+            return "Score record created successfully.";
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public List<Score> readScores() {
+        return scoreRepository.findAll();
+    }
+
+    @Transactional
+    public String updateScore(Score score) {
+        if (scoreRepository.existsByName(score.getName())) {
+            try {
+                List<Score> scores = scoreRepository.findByName(score.getName());
+                scores.stream().forEach(s -> {
+                    Score scoreToBeUpdate = scoreRepository.findById(s.getId()).get();
+                    scoreToBeUpdate.setName(score.getName());
+                    scoreToBeUpdate.setRating(score.getRating());
+                    scoreRepository.save(scoreToBeUpdate);
+                });
+                return "Score record updated.";
+            } catch (Exception e) {
+                throw e;
+            }
+        } else {
+            return "Name does not exists in the database.";
+        }
+    }
+
+    @Transactional
+    public String deleteScore(Score score) {
+        if (scoreRepository.existsByName(score.getName())) {
+            try {
+                List<Score> scores = scoreRepository.findByName(score.getName());
+                scores.stream().forEach(s -> {
+                    scoreRepository.delete(s);
+                });
+                return "Score record deleted successfully.";
+            } catch (Exception e) {
+                throw e;
+            }
+
+        } else {
+            return "Score does not exist";
+        }
+    }
+}
