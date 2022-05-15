4694fa599dc548af2c1be468908fe78e0326f20d
Kaiser, Anton <anton.kaiser@student.fhws.de>
2022-05-15 19:52:33 +0200

    Added ScoreRepository interface to access Score table in DB

---
+++ src/main/java/de/fhws/simplex/ScoreRepository.java
@@ -0,0 +1,20 @@
+package de.fhws.simplex;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScoreRepository extends JpaRepository<Score, Integer> {
+
+    public boolean existsByName(String name);
+
+    public List<Score> findByName(String name);
+
+    @Query("select max(s.rating) from Score s")
+    public Integer findMaxRating();
+
+}
