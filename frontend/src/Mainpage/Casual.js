1c7a3e5a5e078f1008086de3e4e3de638e9658c8
Braun, Hannes <hannes.braun@student.fhws.de>
2022-05-15 18:09:49 +0200

    readied casual.js to display given tables

--- frontend/src/Mainpage/Casual.js
+++ frontend/src/Mainpage/Casual.js
@@ -7,30 +7,42 @@ import Tabellenfeld from "./Tabelle/Tabellenfeld";
 
 /* Hier soll der user das Simplex Problem lösen können */
 
+
 function Casual(onClick) {
     const [Zeilen, setZeilen] = useState('');
     const [outZeilen, setOutZeilen] = useState(0)
     const [Spalten, setSpalten] = useState('');
     const [outSpalten, setOutSpalten] = useState(0);
     const [aktiv, setAktiv] = useState(false);
-
+    const [counter, setCounter] = useState(0);
 
     const onSubmit = (e) => {
         e.preventDefault()
     }
 
     function sendTabelle() {
-
+        //toDO: tabelle in json ändern und senden
     }
+
+
     function addTabelle() {
         setAktiv(true)
         setOutSpalten(Spalten)
         setOutZeilen(Zeilen)
+        setCounter(1);
     }
     function onChange(e){
         setSpalten(e.target.value)
         Tabellenfeld.editable=false
         Tabellenfeld.content=Zeilen
+    }
+    function forward(){
+        //toDo: testen ob am ende der map ist
+        setCounter(counter+1)
+    }
+    function backwards(){
+        //toDo: testen ob auf 1 ist
+        setCounter(counter-1)
     }
 
     return (
@@ -62,19 +74,30 @@ function Casual(onClick) {
                     className='absenden'
                     text='Abfahrt'
                     onClick={sendTabelle}/>
+                {counter > 0 && <Button
+                    className='Rückwärts'
+                    text='Rückwärts'
+                    onClick={backwards}/>}
+                {counter > 0 && <Button
+                    className='Forwärts'
+                    text='Fortwärts'
+                    onClick={forward}/>}
             </div>
             {aktiv && <Tabelle editable={true} Zeileninput={outZeilen} Spalteninput={outSpalten}/>}
+            {counter > 0 && displayGiveTable() }
             <div>
                 <h2>Hier entsteht dann das Trainingsprogramm für den Simplex Trainer</h2>
             </div>
         </form>
 
     )
+    function displayGiveTable() {
+        //toDO: display the Table equal to the counter
+        return false;
+    }
 
 }
 
-export default Casual
-/*
-                   onChange={(e) => setZeilen(e.target.value)}/>
 
-                    */
+
+export default Casual
