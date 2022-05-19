2ecc5529cce394403e13edcaec90c5978e6fa529
Struensee, Fabian <fabian.struensee@student.fhws.de>
2022-05-19 15:53:05 +0200

    Added two tests, RottisBeispielStep One and Two

---
+++ src/test/java/MatrixTest/GCCalculatorTest.java
@@ -0,0 +1,28 @@
+package MatrixTest;
+
+import de.fhws.simplex.Matrix;
+import de.fhws.simplex.GreatestChangeCalculator;
+import de.fhws.simplex.SimplexRankedTables;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class GCCalculatorTest {
+    Matrix m = new Matrix();
+    GreatestChangeCalculator c = new GreatestChangeCalculator();
+
+    @Test
+    public void RottisBeispielStepOne()
+    {
+        m = SimplexRankedTables.beRottisBeispiel();
+        Assert.assertArrayEquals(new int[]{2, 0}, c.getPivotelement(m.getMatrix()));
+
+    }
+    @Test
+    public void RottisBeispielStepTwo()
+    {
+        m = SimplexRankedTables.beRottisBeispiel();
+        m = m.nextStep(new int[]{2, 0});
+        Assert.assertArrayEquals(new int[]{1, 1}, c.getPivotelement(m.getMatrix()));
+
+    }
+}
