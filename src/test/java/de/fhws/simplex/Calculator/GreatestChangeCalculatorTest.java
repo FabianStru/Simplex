ebb2a30ce368c5c9ef9afe587f3289b797c4ea61
Kaiser, Anton <anton.kaiser@student.fhws.de>
2022-05-22 18:50:55 +0200

    Added docstrings, renamed methods, added assertException to SteepestUnitAscendCalculatorTest (made assertException static because of this)

--- src/test/java/de/fhws/simplex/Calculator/GreatestChangeCalculatorTest.java
+++ src/test/java/de/fhws/simplex/Calculator/GreatestChangeCalculatorTest.java
@@ -2,9 +2,12 @@ package de.fhws.simplex.Calculator;
 
 import de.fhws.simplex.Simplex.Matrix;
 import de.fhws.simplex.Simplex.SimplexRankedTables;
+import de.fhws.simplex.Simplex.SimplexRankedTablesEnum;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static de.fhws.simplex.Simplex.SimplexRankedTablesEnum.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -17,56 +20,49 @@ public class GreatestChangeCalculatorTest {
     GreatestChangeCalculator c = new GreatestChangeCalculator();
 
     /**
-     * Tests the matrix SimplexRankedTables.beRottisBeispiel and whether it
+     * Tests the matrix Beispiel1 and whether it
      * finds the correct pivot elements according to the Greatest change procedure.
      * Also tests if it throws an IndexOutOfBoundsException
      * in case it's called again after the solution is found since no -1 is then returned as "index".
      */
     @Test
-    public void test_RottisBeispiel() {
-        m = SimplexRankedTables.beRottisBeispiel();
-        m.printMatrix();
-        Assert.assertArrayEquals(new int[]{2, 0}, c.getPivotelement(m.getMatrix()));
+    public void test_GreatestChange_RottisBeispiel() {
+        m = Beispiel1.getMatrix();
+        Assertions.assertArrayEquals(new int[]{2, 0}, c.getPivotelement(m.getMatrix()));
         m = m.nextStep(new int[]{2, 0});
-        m.printMatrix();
-        Assert.assertArrayEquals(new int[]{1, 1}, c.getPivotelement(m.getMatrix()));
+        Assertions.assertArrayEquals(new int[]{1, 1}, c.getPivotelement(m.getMatrix()));
         m = m.nextStep(new int[]{1, 1});
-        m.printMatrix();
-        assertException();
+        assertException(c, m);
 
     }
 
     /**
-     * Tests the matrix SimplexRankedTables.beRottisBeispiel2 and whether it
+     * Tests the matrix Beispiel2 and whether it
      * finds the correct pivot elements according to the Greatest change procedure.
      * Also tests if it throws an IndexOutOfBoundsException
      * in case it's called again after the solution is found since no -1 is then returned as "index".
      */
     @Test
-    public void test_RottisBeispiel2() {
-        m = SimplexRankedTables.beRottisBeispiel2();
-        m.printMatrix();
-        Assert.assertArrayEquals(new int[]{3, 1}, c.getPivotelement(m.getMatrix()));
+    public void test_GreatestChange_RottisBeispiel2() {
+        m = Beispiel2.getMatrix();
+        Assertions.assertArrayEquals(new int[]{3, 1}, c.getPivotelement(m.getMatrix()));
         m = m.nextStep(new int[]{3, 1});
-        m.printMatrix();
-        assertException();
+        assertException(c, m);
 
     }
 
     /**
-     * Tests the matrix SimplexRankedTables.beRottisBeispiel3 and whether it
+     * Tests the matrix Beispiel3 and whether it
      * finds the correct pivot elements according to the Greatest change procedure.
      * Also tests if it throws an IndexOutOfBoundsException
      * in case it's called again after the solution is found since no -1 is then returned as "index".
      */
     @Test
-    public void test_RottisBeispiel3() {
-        m = SimplexRankedTables.beRottisBeispiel3();
-        m.printMatrix();
-        Assert.assertArrayEquals(new int[]{1, 1}, c.getPivotelement(m.getMatrix()));
+    public void test_GreatestChange_RottisBeispiel3() {
+        m = Beispiel3.getMatrix();
+        Assertions.assertArrayEquals(new int[]{1, 1}, c.getPivotelement(m.getMatrix()));
         m = m.nextStep(new int[]{1, 1});
-        m.printMatrix();
-        assertException();
+        assertException(c, m);
 
     }
 
@@ -74,7 +70,7 @@ public class GreatestChangeCalculatorTest {
      * Check if ArrayIndexOutOfBoundsException is thrown and
      * the included error message includes -1.
      */
-    public void assertException() {
+    public static void assertException(Calculator c, Matrix m) {
         Exception exception = assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
             c.getPivotelement(m.getMatrix());
         });
