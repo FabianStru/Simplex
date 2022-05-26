f7fa8dab7b9a201fa295cbf7c6951050a441124e
Braun, Hannes <hannes.braun@student.fhws.de>
2022-05-26 10:37:12 +0200

    /rankings and /tutorial added in site path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from "./Mainpage/Home";
 import Casual from "./Mainpage/Casual";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Ranked from './Mainpage/Ranked';
+import Rankings from './Mainpage/Rankings';
 
 
 
@@ -19,6 +20,8 @@ function App() {
                         <Route path="/" element={(<Home/>)}/>
                         <Route path="/play" element={(<Casual/>)}/>
                         <Route path='/ranked' element={(<Ranked/>)}/>
+                        <Route path='/rankings' element={(<Rankings/>)}/>
+                        <Route path='/tutorial' element={(<Tutorial/>)}/>
 
                         {/* das wird angezeigt wenn die Falsche URl eingegeben wird. No touchy, als ich das erstellt hab gings auf einmal sonst bugts  */}
                         <Route path="/*" element={(<h1>This is the wrong page</h1>)}/>
