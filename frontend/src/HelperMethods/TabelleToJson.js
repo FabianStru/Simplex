d5e6a5b1cff9a11cc16e56b06a459a8bb99c5bcf
Braun, Hannes <hannes.braun@student.fhws.de>
2022-06-02 15:13:16 +0200

    Fixed error in TabelleToJson. Array doenst go out of bounds anymore

--- frontend/src/HelperMethods/TabelleToJson.js
+++ frontend/src/HelperMethods/TabelleToJson.js
@@ -5,7 +5,7 @@ function TabelleToJson(arrayOfMatrix) {
     const finalData = [{}];
     //tempMatrix ist ein temoräres objekt was am ende der Loop in finalData geschoben wird
     const tempObject = {}
-    const tempMatrix = [[], []]
+    const tempMatrix = []
 
     //zuerst gehe ich durch jede Matrix:
     for (let i = 0; i < arrayOfMatrix.length; i++) {
@@ -14,19 +14,27 @@ function TabelleToJson(arrayOfMatrix) {
         //initialisiere rowHeader und füllen mit dem inhalt der obersten zeile der Matrix:
         let rowHeader = fillRowheader(i);
 
-        //jetzt gehen wir durch jede Matrix, zeile für Zeile
+        //jetzt gehen wir Zeile für Zeile durch die Matrix i
         for (let j = 0; j < arrayOfMatrix[i]; j++) {
 
             if (i > 0) { //ersten index auslassen und columHeader updaten
                 columnHeader.push(arrayOfMatrix[i][0])
             }
+            if (j > 0) {
+                tempMatrix.push(arrayOfMatrix[i][j])
+            }
         }
+        console.log('matrix: ' + tempMatrix)
+        console.log('columnHeader: ' + columnHeader)
+        console.log('rowHeader: ' + rowHeader)
+        // TempMatrix, RowHeader und columnheader zu einem Objekt machen und zu finalData hinzufügen:
+
     }
     return finalData;
 
 
     function fillRowheader(i) {
-        let rowHeader = [arrayOfMatrix.length - 1];
+        let rowHeader = [arrayOfMatrix[0].length - 1];
         for (let j = 0; j < arrayOfMatrix[i][0]; j++) {
             if (j > 0) {
                 rowHeader = arrayOfMatrix[i][j];
