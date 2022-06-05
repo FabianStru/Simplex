7a841ae3791b26db0360e2e51e08d80e0caf1209
Kaiser, Anton <anton.kaiser@student.fhws.de>
2022-06-05 19:09:40 +0200

    Commented ScoreMatchingController to avoid Spring Boot startup issues

--- src/main/java/de/fhws/simplex/Controller/ScoreMatchingController.java
+++ src/main/java/de/fhws/simplex/Controller/ScoreMatchingController.java
@@ -1,40 +1,40 @@
-package de.fhws.simplex.Controller;
-
-import de.fhws.simplex.Repository.ScoreMatching;
-import de.fhws.simplex.Service.ScoreMatchingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-public class ScoreMatchingController {
-
-    @Autowired
-    private ScoreMatchingService scoreMatchingService;
-
-    @GetMapping("info")
-    public String info() {
-        return "The application is up...";
-    }
-
-/*    @RequestMapping(value = "createscoreMatching", method = RequestMethod.POST)
-    public String createScoreMatching(@RequestBody Score scoreMatching){
-        return scoreMatchingService.createScoreMatching(scoreMatching);
-    }*/
-
-    @GetMapping("readscoreMatchings")
-    public List<ScoreMatching> readScoreMatchings() {
-        return scoreMatchingService.readScoreMatchings();
-    }
-
-/*    @RequestMapping(value = "updatescoreMatching", method = RequestMethod.PUT)
-    public String updateStudet(@RequestBody ScoreMatching scoreMatching){
-        return scoreMatchingService.updateScoreMatching(scoreMatching);
-    }
-
-    @RequestMapping(value = "deletescoreMatching", method = RequestMethod.DELETE)
-    public String deleteScoreMatching(@RequestBody ScoreMatching scoreMatching){
-        return scoreMatchingService.deleteScoreMatching(scoreMatching);
-    }*/
-}
+//package de.fhws.simplex.Controller;
+//
+//import de.fhws.simplex.Repository.ScoreMatching;
+//import de.fhws.simplex.Service.ScoreMatchingService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//public class ScoreMatchingController {
+//
+//    @Autowired
+//    private ScoreMatchingService scoreMatchingService;
+//
+//    @GetMapping("info")
+//    public String info() {
+//        return "The application is up...";
+//    }
+//
+///*    @RequestMapping(value = "createscoreMatching", method = RequestMethod.POST)
+//    public String createScoreMatching(@RequestBody Score scoreMatching){
+//        return scoreMatchingService.createScoreMatching(scoreMatching);
+//    }*/
+//
+//    @GetMapping("readscoreMatchings")
+//    public List<ScoreMatching> readScoreMatchings() {
+//        return scoreMatchingService.readScoreMatchings();
+//    }
+//
+///*    @RequestMapping(value = "updatescoreMatching", method = RequestMethod.PUT)
+//    public String updateStudet(@RequestBody ScoreMatching scoreMatching){
+//        return scoreMatchingService.updateScoreMatching(scoreMatching);
+//    }
+//
+//    @RequestMapping(value = "deletescoreMatching", method = RequestMethod.DELETE)
+//    public String deleteScoreMatching(@RequestBody ScoreMatching scoreMatching){
+//        return scoreMatchingService.deleteScoreMatching(scoreMatching);
+//    }*/
+//}
