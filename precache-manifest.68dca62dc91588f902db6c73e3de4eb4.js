9d7438243d7dfb8f29067cdd4807f06fdb1c9ea4
Struensee, Fabian <fabian.struensee@student.fhws.de>
2022-06-10 20:42:16 +0200

    Updates

---
+++ precache-manifest.68dca62dc91588f902db6c73e3de4eb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c1f1ac9a9e45b7b58e3",
+    "url": "/Simplex/static/css/main.a7c64008.chunk.css"
+  },
+  {
+    "revision": "7c1f1ac9a9e45b7b58e3",
+    "url": "/Simplex/static/js/main.38f00178.chunk.js"
+  },
+  {
+    "revision": "979c5ba69672b6a258b6",
+    "url": "/Simplex/static/js/runtime~main.34e8d7d7.js"
+  },
+  {
+    "revision": "2829e358398d7a84bf1a",
+    "url": "/Simplex/static/js/2.c045a719.chunk.js"
+  },
+  {
+    "revision": "77ff2abf6da91ad83241",
+    "url": "/Simplex/static/js/3.153d6254.chunk.js"
+  },
+  {
+    "revision": "9d4bf20bdee8dd2eacc65ec9cbb20d67",
+    "url": "/Simplex/static/media/Icon.9d4bf20b.svg"
+  },
+  {
+    "revision": "8ba934a21ff646c82e4812facff5e464",
+    "url": "/Simplex/static/media/Pfeil.8ba934a2.svg"
+  },
+  {
+    "revision": "0a1cde5ba0b1ac135bf6899044e54a27",
+    "url": "/Simplex/static/media/Icon2.0a1cde5b.svg"
+  },
+  {
+    "revision": "f52940276f94ae19d39a2e4382f5fbab",
+    "url": "/Simplex/index.html"
+  }
+];
